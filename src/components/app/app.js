4e9cbc2fab268abf9454e2e06804a1f3d902f105
Vladislav Morgunov <87144955+Zoncovsky@users.noreply.github.com>
2024-02-10 19:42:22 +0200

    add like fuctional

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,11 +9,11 @@ import './app.css';
 function App() {
 
   const data = [
-    {name: 'Jogn W.', salary: 800, increase: false, id: 1},
-    {name: 'Micahel J.', salary: 8000, increase: true, id: 2},
-    {name: 'Kendrick L.', salary: 3000, increase: false, id: 3},
-    {name: 'Victor P.', salary: 5000, increase: false, id: 4},
-    {name: 'Vlad M.', salary: 12000, increase: true, id: 5},
+    {name: 'Jogn W.', salary: 800,  id: 1},
+    {name: 'Micahel J.', salary: 8000, id: 2},
+    {name: 'Kendrick L.', salary: 3000,  id: 3},
+    {name: 'Victor P.', salary: 5000,  id: 4},
+    {name: 'Vlad M.', salary: 12000, id: 5},
   ];
 
   return (
