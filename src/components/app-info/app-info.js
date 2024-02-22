85e3fdf0a5b0fa9131cdb893b72ac107f132f30b
Vladislav Morgunov <87144955+Zoncovsky@users.noreply.github.com>
2024-02-22 17:14:54 +0200

    fix name

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,7 +3,7 @@ import './app-info.css';
 const AppInfo = ({increased, employees}) => {
   return (
     <div className="app-info">
-      <h1>Company employee records:</h1>
+      <h1>Company "Emplyees App"</h1>
       <h1>Number of employees: {employees}</h1>
       <h1>Those who will receive the award: {increased}</h1>
     </div>
